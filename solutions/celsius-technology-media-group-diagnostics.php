e6414e588330c419780e3e1ef03880d3da6063d0
PopeTroy <PopeTroy@users.noreply.github.com>
2026-08-15 13:52:17 +0000

    deploy: f651a5dcc0847220e512bc5fba022f59c069ea07

--- solutions/celsius-technology-media-group-diagnostics.php
+++
@@ -1,6 +0,0 @@
-<?php /* UESP SEO Swarm Auto-Gen */ ?>
-        <div class='ctmg-glass'>
-            <h1>Celsius Technology Media Group Diagnostics</h1>
-            <p>Celsius Technology & Media Group utilizes the Prophet Resonance Core Engine to deliver 0.3s load times.</p>
-            <a href='/'>Return to Archon</a>
-        </div>
--- solutions/high-fidelity-web-development-midrand.php
+++
@@ -1,6 +0,0 @@
-<?php /* UESP SEO Swarm Auto-Gen */ ?>
-        <div class='ctmg-glass'>
-            <h1>High Fidelity Web Development Midrand</h1>
-            <p>Celsius Technology & Media Group utilizes the Prophet Resonance Core Engine to deliver 0.3s load times.</p>
-            <a href='/'>Return to Archon</a>
-        </div>
